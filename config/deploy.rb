f527c92fd36ee9d1ad58403eaafdf9074d54b86f
Jason Malcolm <jaciones@gmail.com>
2015-11-16 15:58:19 -0800

    make key env var

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,12 @@
 puts "Vlad loading..."
 set :application, "dotforum"
-set :ssh_flags, %w[ -i /Users/jason/workspace/topspectrum/keys/default.pem]
+
+key_location = ENV["EC2_KEY"]
+unless key_location
+	p "You must have environment variable EC2_KEY set to location of ec2 key"
+	exit 1
+end
+set :ssh_flags, [ "-i", "#{key_location}"]
 
 set :domain, "ubuntu@ec2-54-218-52-233.us-west-2.compute.amazonaws.com"
 set :deploy_to, "/srv/topspectrum/dotforum"
@@ -14,3 +20,5 @@ set :revision, "origin/master"
 #  set :domain,    "example.com"
 #  set :deploy_to, "/path/to/install"
 #end
+
+#/Users/jason/workspace/topspectrum/keys/default.pem
